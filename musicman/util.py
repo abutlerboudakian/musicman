0311ae305ad017073c668c78f19c3ffa84d4a9fe
abutlerboudakian <abutlerboudakian@gmail.com>
2021-10-25 19:20:08 -0400

    Update util.py

--- musicman/util.py
+++ musicman/util.py
@@ -90,6 +90,9 @@ def handle_spotify(client: str, secret: str, url: str):
             (
                 f'{track["name"]} '
                 f'{track["artists"][0]["name"] if len(track["artists"]) > 0 else ""}'  # noqa: E501
+            ) if item_type.lower() == 'album' else (
+                f'{track["track"]["name"]} '
+                f'{track["track"]["artists"][0]["name"] if len(track["track"]["artists"]) > 0 else ""}'  # noqa: E501
             )
             for track in tracks
         ]
